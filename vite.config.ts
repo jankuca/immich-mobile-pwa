b36da72432bbfbbd60a4052d6787a5b7eb31dd7b
jankuca <jan@jankuca.com>
2025-04-30 21:51:55 +0200

    add preact react compat resolver config to vite

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,14 @@ import preact from '@preact/preset-vite';
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [preact()],
+	resolve: {
+		alias: {
+			'react': 'preact/compat',
+			'react-dom/test-utils': 'preact/test-utils',
+			'react-dom': 'preact/compat',
+			'react/jsx-runtime': 'preact/jsx-runtime'
+		}
+	},
 	server: {
 		// Enable access from all network interfaces
 		host: '0.0.0.0',
